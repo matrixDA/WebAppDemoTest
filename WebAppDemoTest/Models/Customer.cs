852221f7f5343e7790f1c13f4ac0c485593b602b
adhik <adhik@DINESH>
2023-11-15 12:14:04 -0500

    initial commit

---
+++ WebAppDemoTest/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace WebAppDemoTest.Models
+{
+    public class Customer
+    {
+        int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+
+
+    }
+}
